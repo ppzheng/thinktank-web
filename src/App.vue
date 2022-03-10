32fd0fb78033584a8f587318375b908511f1aea8
zhengxin <zx_00s@163.com>
2022-03-10 22:36:10 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Hello msg="Welcome to thinktank"/>
+</template>
+
+<script>
+import Hello from './components/index.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Hello
+  }
+}
+</script>
+
---
+++ src/components/index.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="hello">
+    ThinkTank ^_^ 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .hello{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+    font-weight: 500;
+    font-size: 24px;
+    color: blueviolet;
+  }
+</style>
